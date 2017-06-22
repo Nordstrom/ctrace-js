fd738acdb8aa2fcb8b3518a92021855ac437301a
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-06-22 13:48:08 -0700

    Added log level functions and unit test

--- src/index.js
+++ src/index.js
@@ -7,4 +7,19 @@ const tracer = Object.assign(new GlobalTracer(), opentracing)
 tracer.express = express
 tracer.request = request
 
+function logFn (level) {
+  return function log (ctx, event, data) {
+    if (level === 'debug') {
+      data = Object.assign({ debug: true }, data)
+    } else if (level === 'error') {
+      data = Object.assign({ error: true }, data)
+    }
+    ctx.span.log(Object.assign({ event: event , level: level}, data))
+  }
+}
+tracer.debug = logFn('debug')
+tracer.info = logFn('info')
+tracer.warn = logFn('warn')
+tracer.error = logFn('error')
+
 export default tracer
